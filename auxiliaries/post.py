531882a5bfe4fd6ca6c09162a038efae1b1cafb4
SickAndTired <78124142+S1ckAndTired@users.noreply.github.com>
2023-11-02 05:46:17 -0300

    Update post.py

--- auxiliaries/post.py
+++ auxiliaries/post.py
@@ -5,7 +5,7 @@ from auxiliaries.common_stuff import *
 from auxiliaries.requester import issuer
 from auxiliaries.rate_limting import *
 from urllib.parse import quote
-def post_it(target, wordlist, body, proxies, filter_error, filter_size, delay):
+def post_it(target, wordlist, body, proxies, filter_error, filter_size, delay, rate):
     if "Content-Type" in custom_headers.keys():
         if "json" in custom_headers["Content-Type"] or "javascript" in custom_headers["Content-Type"]:
             f = open(wordlist, "r")
@@ -13,7 +13,7 @@ def post_it(target, wordlist, body, proxies, filter_error, filter_size, delay):
                 last = words.strip().split()[-1]
                 fuzz = words.strip()
                 data = (f"{body.replace('FUZZ', fuzz)}")
-                issuer(target, data, fuzz, proxies, filter_error, filter_size, delay)
+                issuer(target, data, fuzz, proxies, filter_error, filter_size, delay, rate)
             last_worda.append(last)
         elif "urlencoded" in custom_headers["Content-Type"]:
             f = open(wordlist, "r")
@@ -23,13 +23,13 @@ def post_it(target, wordlist, body, proxies, filter_error, filter_size, delay):
                 if "&" in fuzz:
                     fuzz = quote(fuzz)
                     data = body.replace("FUZZ", fuzz)
-                    issuer(target, data, fuzz, proxies, filter_error, filter_size, delay)
+                    issuer(target, data, fuzz, proxies, filter_error, filter_size, delay, rate)
                 else:
                     data = body.replace("FUZZ", fuzz)
-                    issuer(target, data, fuzz, proxies, filter_error, filter_size, delay)
+                    issuer(target, data, fuzz, proxies, filter_error, filter_size, delay, rate)
             last_word.append(last)
             if last_word:
-                time_slip(target, data, fuzz, proxies, filter_error, filter_size, delay)
+                time_slip(target, data, fuzz, proxies, filter_error, filter_size, delay, rate)
         else:
             print("[-] OOPS!! Something else came up. Gotta implement it!!!")
     else:
