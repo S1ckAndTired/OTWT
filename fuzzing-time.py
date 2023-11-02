fa9aa276c05c559fb7a5955eece87cef9615761b
SickAndTired <78124142+S1ckAndTired@users.noreply.github.com>
2023-11-02 05:45:40 -0300

    Update fuzzing-time.py

--- fuzzing-time.py
+++ fuzzing-time.py
@@ -18,7 +18,8 @@ def blah():
     parser.add_argument("-x", "--proxy", metavar="", help="http://<ip>:<port>")
     parser.add_argument("-fr", "--filter-error", dest="filter_error", metavar="", help="Filter by given string")
     parser.add_argument("-fs", "--filter-size", dest="filter_size", metavar="", help="Filter by given size")
-    parser.add_argument("-dl", "--delay", metavar="", help="Set it to avoid rate limiting protections")
+    parser.add_argument("-dl", "--delay", metavar="", help="Delayu between requests")
+    parser.add_argument("-rt", "--rate-limiting", dest="rate", metavar="", help="Given default text in rate-limiting responses")
     args = parser.parse_args()
     headers = args.headers
     data = args.data
@@ -28,23 +29,25 @@ def blah():
     filter_error = args.filter_error
     filter_size = args.filter_size
     delay = args.delay
+    rate = args.rate
+
     if proxy is None:
         proxies = None
         if data:
-            iterator(target, headers, wordlist, data, proxies, filter_error, filter_size, delay)
+            iterator(target, headers, wordlist, data, proxies, filter_error, filter_size, delay, rate)
         else:
             print("NOT READY YET!! NOT SURE WHAT THIS IS FOR!")
     else:
         if target.startswith("https://"):
             proxies = {"https": f"{proxy}"}
             if data:
-                iterator(target, headers, wordlist, data, proxies, filter_error, filter_size, delay)
+                iterator(target, headers, wordlist, data, proxies, filter_error, filter_size, delay, rate)
             else:
                 print("NOT READY YET!! ONLY POST REQUESTS SUPPORTED!")
         else:
             proxies = {"http": f"{proxy}"}
             if data:
-                iterator(target, headers, wordlist, data, proxies, filter_error, filter_size, delay)
+                iterator(target, headers, wordlist, data, proxies, filter_error, filter_size, delay, rate)
             else:
                 print("NOT READY YET!! ONLY POST REQUESTS SUPPORTED!")
 
